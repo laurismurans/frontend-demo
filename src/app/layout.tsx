0a524b753c2ca908e728569a49902275d5bba257
Lauris Murans <lauris.muraans@gmail.com>
2024-09-20 15:48:33 +0200

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { Header } from "@/components/layout/header";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Demo Frontend",
+  description: "Search UI Demo",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <main className="min-h-screen gap-16  font-[family-name:var(--font-geist-sans)]">
+          <Header />
+          <div className="mt-8 px-16 ">{children}</div>
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Search from "@/components/search/search-form";
+import { JSX } from "react/jsx-runtime";
+
+export default function HomePage(): JSX.Element {
+  return (
+    <>
+      <Search />
+    </>
+  );
+}
